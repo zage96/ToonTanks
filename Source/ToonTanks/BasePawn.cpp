9124960aafedae43ef0c7ad554438f9035a0a85e
Enrique Zaragoza <zarag.enrique@gmail.com>
2024-03-26 19:33:46 -0500

    Section 5: 112-118
    Created BasePawn.cpp and BasePawn.c
    Created folder for blueprints and pawns.
    Created 2 BP, BP_PawnTurret and BP_PawnTank
    BasePawn.cpp: Added "Components/CapsuleComponent.h", capsulecomp created in the constructor.. Made the capsuleComp the root component. In constructor, initialized BaseMesh, TurretMesh  and ProjectileSpawnPoint and attached one to another in that order.
    BasePawn.h: CapsuleComp, BaseMesh, TurretMesh  and ProjectileSpawnPoint added in private.

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Components/CapsuleComponent.h"
 #include "BasePawn.h"
 
 // Sets default values
@@ -8,6 +8,22 @@ ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create a capsule component and make it the root component
+	CapsuleComp =  CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	// Create a static mesh component and attach it to the previous capsule component
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	// Create a static mesh component and attach it to the previous base mesh
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	// Create a scene component and attach it to the previous turret mesh
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 
 }
 
